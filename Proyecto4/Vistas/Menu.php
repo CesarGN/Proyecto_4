e80a755714e6e4c3d8d29d08c646e87e2908ccfc
CesarGN <krauser_csr@hotmail.com>
2013-12-12 12:11:53 -0600

    Primero

---
+++ Proyecto4/Vistas/Menu.php
@@ -0,0 +1,37 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="../CSS/General.css" rel="stylesheet">
+        <link href="../CSS/Menu.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="imagen"></div>
+        <div id="formulario">
+            <div id="titulo">Men&uacute;!!!</div>
+            <div class="centro">
+                <table>
+                    <tr>
+                    <button>
+                        Crear tarea
+                    </button>
+                    </tr>
+                    <tr>
+                    <button>
+                        Revisar
+                    </button>
+                    </tr>
+                    <tr>
+                    <button>
+                        
+                    </button>
+                    </tr>
+                </table>    
+            </div>
+
+        </div>
+    </body>
+</html>
---
+++ Proyecto4/Vistas/Registro.php
@@ -0,0 +1,44 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="../CSS/General.css" rel="stylesheet">
+        <link href="../CSS/Registro.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="imagen"></div>
+        <div id="formulario">
+            <div id="titulo">Registrate!!!</div>
+            <form id= "formu"  method="post" >
+                <div class="centro">
+                    
+                    <table>
+                        <tr>
+                            <td><label for="nombre">Nombre</label></td>
+                            <td><input id = "nombre" name = "nombre" type="text"/></td>
+                        </tr>
+                        <tr>
+                            <td><label for="nombre">Usuario</label></td>
+                            <td><input id = "usuario" name = "usario" type="text"/></td>
+                        </tr>
+                        <tr>
+                            <td><label for="password">Password</label></td>
+                            <td><input id = "password" name = "password" type="password"/></td>
+                        </tr>
+                        <tr>
+                            <td><label for="password2">Confirma password</label></td>
+                            <td><input id = "password2" name = "password2" type="password"/></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><div></div><button id="entrar" name="entrar" type="submit" value="Entrar">Entrar</button></td>
+                        </tr>
+                    </table>    
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
