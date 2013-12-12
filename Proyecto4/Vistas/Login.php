dcd33df6d5d6782c58e627e2d50db074505442d4
OmarLS <dragons2113@hotmail.com>
2013-12-12 12:39:35 -0600

    Nuevo

---
+++ Proyecto4/Vistas/Login.php
@@ -0,0 +1,41 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="../CSS/estilos.css" />
+    </head>
+    <body>
+        <div id="logins">
+            <h2>INGRESA A TU AGENDA</h2>
+            <div class="centro">
+           <form id="form" action="" method="post">
+                <fieldset>
+                    <p>
+                        <label for="usuario">Usuario</label>
+                        <input id="usuario" name="usuario" type="text" class="text"/>
+                    </p>
+
+                    <p>
+                        <label for="password">Password</label>
+                        <input id="password" name="password" type="password" class="text"/>
+                    </p>
+
+                    
+                    <button id="entrar" type="submit" value="Iniciar"></button>
+                    
+                </fieldset>
+            </form>
+                </div>
+        </div>
+        
+        <div id="loginsimagen">
+            
+           <form id="form" action="" method="post">
+                
+            </form>
+        </div>
+    </body>
+</html>
